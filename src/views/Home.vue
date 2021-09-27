31dc7286212fe10480a294a1fddd6c1d209fe09f
monmima <monmima@protonmail.com>
2021-09-27 13:49:44 -0400

    Home page now shows pictures of characters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,39 @@
 
 <template>
   <div class="about">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Logo Pokémon">
     <h1>This is the Home page</h1>
     <p>{{ this.$route.params.id }}</p>
     <p>{{ this.$route.params.curOffSet }}</p>
     
     <hr>
 
+
+
     <div>
-      <button @click="removeOffset">(===</button>
-      <button @click="addOffset">===)</button>
+      <!-- {{ arrPokemons }} -->
     </div>
 
-
-    <ul v-if="response.length > 0">
-      <li v-for="(item, index) in response" :key="`${item.title}${index}`">
+    <div class="pokegrid" v-if="arrPokemons.length > 0">
+      <!-- <li v-for="(item, index) in response" :key="`${item.title}${index}`">
           <router-link :to="`/about/${item.url.split('/')[6]}`" target="_blank" :title="item.name">{{ item.name }}</router-link>
-      </li>
-    </ul>
-    <ul v-else>
+      </li> -->
+
+        <figure v-for="(item, index) in arrPokemons" :key="`${item.name}${index}`">
+          <img :src="`${item.sprites.front_default}`" :alt="`Image of ${item.name}`">
+          <router-link :to="`/about/${index}`" target="_blank" :title="item.name">{{ item.name }}</router-link>
+        </figure>
+
+    </div>
+
+    <div v-else>
       Loading...
-    </ul>
+    </div>
+
+    <div>
+      <button @click="addOffset">I want more Pokémons</button>
+    </div>
 
 
   </div>
@@ -36,7 +48,8 @@
             return {
                 response: [],
                 offset: 0,
-                responseOne: []
+                responseOne: [],
+                arrPokemons: []
             }
         },
         methods: {
@@ -52,47 +65,45 @@
             this.fetchData(this.offset);
           },
 
+
           fetchData(curOffset) {
             console.log(curOffset);
+            
+            for (let i = curOffset + 1; i < curOffset + 19; i++) {
+              
+              fetch(`https://pokeapi.co/api/v2/pokemon/` + i)
+                  .then(res => res.json())
 
-            fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${curOffset}`)
-                .then(res => res.json())
+                  .then(data => this.response = data) // pass the data to the response variable
 
-                .then(data => this.response = data.results) // pass the data to the response variable
+                  // .then(console.log("test"))
+                  // .then(console.log("other test"))
 
-                // .then(console.log("test"))
-                // .then(console.log("other test"))
+                  // .then(data => console.log(data)) // print to the console
 
-                .then(data => console.log(data)) // print to the console
+                  .then(data => {
+                    // let x = data; 
+                    // for (let i = 0; i < 10; i++) {
+                    //   console.log(data);
+                    this.arrPokemons.push(data);
+                    // console.log(this.arrPokemons);
+                  })
 
-            .catch(function (err) {
-                console.log('ERROR CATCHED - ' + err);
-            });
-          },
+              .catch(function (err) {
+                  console.log(`ERROR CATCHED on line ${i}: ${err}`);
+              });
 
-          fetchOne(id) {
+            } // end of for loop
 
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-                .then(res => res.json())
+          } // end of fetch data
 
-                .then(data => this.responseOne = data) // pass the data to the response variable
 
-                // .then(console.log("test"))
-                // .then(console.log("other test"))
-
-                .then(data => console.log(data)) // print to the console
-
-            .catch(function (err) {
-                console.log('ERROR CATCHED - ' + err);
-            });
-
-          }
 
 
         },
 
         mounted: function() {
-          this.fetchData(this.curOffset);
+          this.fetchData(this.offset);
         }
 
     });
@@ -103,5 +114,21 @@
   button {
     margin: 20px;
     padding: 5px;
+  }
+
+  figure {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  figure img {
+    max-width: 96px;
+  }
+
+  .pokegrid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
   }
 </style>
